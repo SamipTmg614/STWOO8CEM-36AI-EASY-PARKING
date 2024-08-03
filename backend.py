4e25bd980bb89fa85ce9a33d0b46966443d84969
Sujal Maharjan <sujalmhrz69@gmail.com>
2024-08-03 22:19:22 +0545

    location button linked

--- backend.py
+++ backend.py
@@ -4,28 +4,28 @@ import database
 root=Tk()
 root.geometry("1920x880")
 
-
-
-def green(button,x):
+spaces=None
+#Function to change button colour and set values NULL again
+def green(button,x,location):
     conn=database.makeconnection()
     c=conn.cursor()
     button.config(text="available",bg="green")
-    c.execute(f'''UPDATE availability
+    c.execute(f'''UPDATE {location}
                     SET status=?,model=NULL,number=NULL
                     WHERE id=? 
                   ''',("TRUE",x))
     conn.commit()
     conn.close()
     
-#Function to change the button colour and text
-def red(button,x):
+#Function to change the button colour and update details ralted to the button
+def red(button,x,location):
     conn=database.makeconnection()
     c=conn.cursor()
     button.config(text="booked",bg="red")
     model=model_var.get()
     number=number_var.get()
 
-    c.execute(f'''UPDATE availability
+    c.execute(f'''UPDATE {location}
                     SET status=?,model=?,number=?
                     WHERE id=? 
                 ''',("FALSE",model,number,x))
@@ -33,17 +33,19 @@ def red(button,x):
     conn.commit()
     conn.close()
    
-def check_availability(button,x):
+#Function to check if the slot is available or not
+def check_slot(button,x,location):
+    database.location_table(location)
     conn=database.makeconnection()
     c=conn.cursor()
 
     #Code to change the colour and text of the button according to the status
-    c.execute("SELECT status from availability WHERE id=?",(x,))
+    c.execute(f"SELECT status from {location} WHERE id=?",(x,))
     status=c.fetchone()
     if status[0]=="FALSE":
         win=Toplevel()
         win.geometry("300x300")
-        c.execute("SELECT model,number FROM availability WHERE id=?",x)
+        c.execute(f"SELECT model,number FROM {location} WHERE id=?",x)
         details=c.fetchmany()
 
         detail_label=Label(win,text="Details")
@@ -55,7 +57,7 @@ def check_availability(button,x):
         time_label=Label(win,text="Entry Time:")
         time_label.place(x=0,y=60)
 
-        confirm=Button(win,text="Confirm",command=lambda:[green(button,x),win.destroy()])
+        confirm=Button(win,text="Confirm",command=lambda:[green(button,x,location),win.destroy()])
         confirm.place(x=65,y=80)
         win.mainloop()
     else:
@@ -81,26 +83,28 @@ def check_availability(button,x):
         time_label=Label(win,text=f"Time:  {time}")
         time_label.place(x=10,y=50)
 
-        submit=Button(win,text="Submit",command=lambda:[red(button,x),win.destroy()])
+        submit=Button(win,text="Submit",command=lambda:[red(button,x,location),win.destroy()])
         submit.place(x=80,y=80)
 
         win.mainloop()
 
-    conn.commit()
     conn.close()
 
-def button_status(button,x):
+#Determines colour of the button according to status in database
+def button_status(button,x,location):
     conn=database.makeconnection()
     c=conn.cursor()
-    c.execute("SELECT status from availability WHERE id=?",(x,))
+    c.execute(f"SELECT status from {location} WHERE id=?",(x,))
     status=c.fetchone()
     if status[0]=="TRUE":
-        button.config(text="available",bg="green")  
-    else:
+        button.config(text="available",bg="green") 
+
+    elif status[0]=="FALSE":
         button.config(text="booked",bg="red") 
+
+    conn.commit()
     conn.close() 
 
-database.availability_table()
 
 #Frame for logo
 logo=Frame(root,bg="blue",width=30,height=150,borderwidth=15)
@@ -108,116 +112,123 @@ logo.pack(fill=X)
 lbl1=Label(logo,text="test")
 lbl1.pack()
 
-#Frame for available spaces
-spaces=Frame(root,width=700,height=880,borderwidth=4,relief=GROOVE)
-spaces.pack(side=RIGHT)
-lbl2=Label(spaces,text="Available Spaces",font=("courier",19,"bold"))
-lbl2.place(x=270)
+def create_button(location):
+    global spaces
+    try:
+        spaces.destroy()
+    except:
+        ...
+    database.location_table(location)
+    spaces=Frame(root,width=700,height=880,borderwidth=4,relief=GROOVE)
+    spaces.pack(side=RIGHT)
+    lbl2=Label(spaces,text="Available Spaces",font=("courier",19,"bold"))
+    lbl2.place(x=200)
 
-#32 Buttons for 32 spaces
-b1=Button(spaces,height=2,width=15,command=lambda:check_availability(b1,"1"))
-b1.place(x=20,y=120)
+    #32 Buttons for 32 spaces
+    b1=Button(spaces,height=2,width=15,command=lambda:check_slot(b1,"1",location))
+    b1.place(x=20,y=120)
 
-b2=Button(spaces,height=2,width=15,command=lambda:check_availability(b2,"2"))
-b2.place(x=190,y=120)
+    b2=Button(spaces,height=2,width=15,command=lambda:check_slot(b2,"2",location))
+    b2.place(x=180,y=120)
 
-b3=Button(spaces,height=2,width=15,command=lambda:check_availability(b3,"3"))
-b3.place(x=370,y=120)
+    b3=Button(spaces,height=2,width=15,command=lambda:check_slot(b3,"3",location))
+    b3.place(x=340,y=120)
 
-b4=Button(spaces,height=2,width=15,command=lambda:check_availability(b4,"4"))
-b4.place(x=550,y=120)
+    b4=Button(spaces,height=2,width=15,command=lambda:check_slot(b4,"4",location))
+    b4.place(x=500,y=120)
 
-b5=Button(spaces,height=2,width=15,command=lambda:check_availability(b5,"5"))
-b5.place(x=20,y=200)
+    b5=Button(spaces,height=2,width=15,command=lambda:check_slot(b5,"5",location))
+    b5.place(x=20,y=200)
 
-b6=Button(spaces,height=2,width=15,command=lambda:check_availability(b6,"6"))
-b6.place(x=190,y=200)
+    b6=Button(spaces,height=2,width=15,command=lambda:check_slot(b6,"6",location))
+    b6.place(x=180,y=200)
 
-b7=Button(spaces,height=2,width=15,command=lambda:check_availability(b7,"7"))
-b7.place(x=370,y=200)
+    b7=Button(spaces,height=2,width=15,command=lambda:check_slot(b7,"7",location))
+    b7.place(x=340,y=200)
 
-b8=Button(spaces,height=2,width=15,command=lambda:check_availability(b8,"8"))
-b8.place(x=550,y=200)
+    b8=Button(spaces,height=2,width=15,command=lambda:check_slot(b8,"8",location))
+    b8.place(x=500,y=200)
 
-b9=Button(spaces,height=2,width=15,command=lambda:check_availability(b9,"9"))
-b9.place(x=20,y=280)
+    b9=Button(spaces,height=2,width=15,command=lambda:check_slot(b9,"9",location))
+    b9.place(x=20,y=280)
 
-b10=Button(spaces,height=2,width=15,command=lambda:check_availability(b10,"10"))
-b10.place(x=190,y=280)
-b11=Button(spaces,height=2,width=15,command=lambda:check_availability(b11,"11"))
-b11.place(x=370,y=280)
+    b10=Button(spaces,height=2,width=15,command=lambda:check_slot(b10,"10",location))
+    b10.place(x=180,y=280)
 
-b12=Button(spaces,height=2,width=15,command=lambda:check_availability(b12,"12"))
-b12.place(x=550,y=280)
+    b11=Button(spaces,height=2,width=15,command=lambda:check_slot(b11,"11",location))
+    b11.place(x=340,y=280)
 
-b13=Button(spaces,height=2,width=15,command=lambda:check_availability(b13,"13"))
-b13.place(x=20,y=360)
+    b12=Button(spaces,height=2,width=15,command=lambda:check_slot(b12,"12",location))
+    b12.place(x=500,y=280)
 
-b14=Button(spaces,height=2,width=15,command=lambda:check_availability(b14,"14"))
-b14.place(x=190,y=360)
+    b13=Button(spaces,height=2,width=15,command=lambda:check_slot(b13,"13",location))
+    b13.place(x=20,y=360)
 
-b15=Button(spaces,height=2,width=15,command=lambda:check_availability(b15,"15"))
-b15.place(x=370,y=360)
+    b14=Button(spaces,height=2,width=15,command=lambda:check_slot(b14,"14",location))
+    b14.place(x=180,y=360)
 
-b16=Button(spaces,height=2,width=15,command=lambda:check_availability(b16,"16"))
-b16.place(x=550,y=360)
+    b15=Button(spaces,height=2,width=15,command=lambda:check_slot(b15,"15",location))
+    b15.place(x=340,y=360)
 
-b17=Button(spaces,height=2,width=15,command=lambda:check_availability(b17,"17"))
-b17.place(x=20,y=440)
+    b16=Button(spaces,height=2,width=15,command=lambda:check_slot(b16,"16",location))
+    b16.place(x=500,y=360)
 
-b18=Button(spaces,height=2,width=15,command=lambda:check_availability(b18,"18"))
-b18.place(x=190,y=440)
+    b17=Button(spaces,height=2,width=15,command=lambda:check_slot(b17,"17",location))
+    b17.place(x=20,y=440)
 
-b19=Button(spaces,height=2,width=15,command=lambda:check_availability(b19,"19"))
-b19.place(x=370,y=440)
+    b18=Button(spaces,height=2,width=15,command=lambda:check_slot(b18,"18",location))
+    b18.place(x=180,y=440)
 
-b20=Button(spaces,height=2,width=15,command=lambda:check_availability(b20,"20"))
-b20.place(x=550,y=440)
+    b19=Button(spaces,height=2,width=15,command=lambda:check_slot(b19,"19",location))
+    b19.place(x=340,y=440)
 
-b21=Button(spaces,height=2,width=15,command=lambda:check_availability(b21,"21"))
-b21.place(x=20,y=520)
+    b20=Button(spaces,height=2,width=15,command=lambda:check_slot(b20,"20",location))
+    b20.place(x=500,y=440)
 
-b22=Button(spaces,height=2,width=15,command=lambda:check_availability(b22,"22"))
-b22.place(x=190,y=520)
+    b21=Button(spaces,height=2,width=15,command=lambda:check_slot(b21,"21",location))
+    b21.place(x=20,y=520)
 
-b23=Button(spaces,height=2,width=15,command=lambda:check_availability(b23,"23"))
-b23.place(x=370,y=520)
+    b22=Button(spaces,height=2,width=15,command=lambda:check_slot(b22,"22",location))
+    b22.place(x=180,y=520)
 
-b24=Button(spaces,height=2,width=15,command=lambda:check_availability(b24,"24"))
-b24.place(x=550,y=520)
+    b23=Button(spaces,height=2,width=15,command=lambda:check_slot(b23,"23",location))
+    b23.place(x=340,y=520)
 
-b25=Button(spaces,height=2,width=15,command=lambda:check_availability(b25,"25"))
-b25.place(x=20,y=600)
+    b24=Button(spaces,height=2,width=15,command=lambda:check_slot(b24,"24",location))
+    b24.place(x=500,y=520)
 
-b26=Button(spaces,height=2,width=15,command=lambda:check_availability(b26,"26"))
-b26.place(x=190,y=600)
+    b25=Button(spaces,height=2,width=15,command=lambda:check_slot(b25,"25",location))
+    b25.place(x=20,y=600)
 
-b27=Button(spaces,height=2,width=15,command=lambda:check_availability(b27,"27"))
-b27.place(x=370,y=600)
+    b26=Button(spaces,height=2,width=15,command=lambda:check_slot(b26,"26",location))
+    b26.place(x=180,y=600)
 
-b28=Button(spaces,height=2,width=15,command=lambda:check_availability(b28,"28"))
-b28.place(x=550,y=600)
+    b27=Button(spaces,height=2,width=15,command=lambda:check_slot(b27,"27",location))
+    b27.place(x=340,y=600)
 
-b29=Button(spaces,height=2,width=15,command=lambda:check_availability(b29,"29"))
-b29.place(x=20,y=680)
+    b28=Button(spaces,height=2,width=15,command=lambda:check_slot(b28,"28",location))
+    b28.place(x=500,y=600)
 
-b30=Button(spaces,height=2,width=15,command=lambda:check_availability(b30,"30"))
-b30.place(x=190,y=680)
+    b29=Button(spaces,height=2,width=15,command=lambda:check_slot(b29,"29",location))
+    b29.place(x=20,y=680)
 
-b31=Button(spaces,height=2,width=15,command=lambda:check_availability(b31,"31"))
-b31.place(x=370,y=680)
+    b30=Button(spaces,height=2,width=15,command=lambda:check_slot(b30,"30",location))
+    b30.place(x=180,y=680)
 
-b32=Button(spaces,height=2,width=15,command=lambda:check_availability(b32,"32"))
-b32.place(x=550,y=680)
+    b31=Button(spaces,height=2,width=15,command=lambda:check_slot(b31,"31",location))
+    b31.place(x=340,y=680)
 
-#Mapping the buttons according to status
-button_list=[b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,
-             b11,b12,b13,b14,b15,b16,b17,b18,b19,b20
-             ,b21,b22,b23,b24,b25,b26,b27,b28,b29,b30,b31,b32]
-i=1
-for ele in button_list:
-    button_status(ele,str(i))
-    i+=1
+    b32=Button(spaces,height=2,width=15,command=lambda:check_slot(b32,"32",location))
+    b32.place(x=500,y=680)
+
+    #Mapping the buttons according to status
+    button_list=[b1,b2,b3,b4,b5,b6,b7,b8,b9,b10,
+                b11,b12,b13,b14,b15,b16,b17,b18,b19,b20
+                ,b21,b22,b23,b24,b25,b26,b27,b28,b29,b30,b31,b32]
+    i=1
+    for ele in button_list:
+        button_status(ele,str(i),location)
+        i+=1
 
 
 
@@ -225,40 +236,40 @@ for ele in button_list:
 locations=Frame(root,width=900,height=880,relief=GROOVE,borderwidth=4)
 locations.pack(side=LEFT)
 
-btn=Button(locations,text="Location  1",height=5,width=30,background="red")
+btn=Button(locations,text="Location  1",height=5,width=30,background="red",command=lambda:create_button("Location_1"))
 btn.place(x=30,y=120)
 
-btn1=Button(locations,text="Location 2",height=5,width=30,background="red")
+btn1=Button(locations,text="Location 2",height=5,width=30,background="red",command=lambda:create_button("Location_2"))
 btn1.place(x=300,y=120)
 
-btn3=Button(locations,text="Location 3",height=5,width=30,background="red")
+btn3=Button(locations,text="Location 3",height=5,width=30,background="red",command=lambda:create_button("Location_3"))
 btn3.place(x=570,y=120)
 
-btn4=Button(locations,text="Location 4",height=5,width=30,background="red")
+btn4=Button(locations,text="Location 4",height=5,width=30,background="red",command=lambda:create_button("Location_4"))
 btn4.place(x=30,y=250)
 
-btn5=Button(locations,text="Location 5",height=5,width=30,background="red")
+btn5=Button(locations,text="Location 5",height=5,width=30,background="red",command=lambda:create_button("Location_5"))
 btn5.place(x=300,y=250)
 
-btn6=Button(locations,text="Location 6",height=5,width=30,background="red")
+btn6=Button(locations,text="Location 6",height=5,width=30,background="red",command=lambda:create_button("Location_6"))
 btn6.place(x=570,y=250)
 
-btn7=Button(locations,text="Location 4",height=5,width=30,background="red")
+btn7=Button(locations,text="Location 4",height=5,width=30,background="red",command=lambda:create_button("Location_7"))
 btn7.place(x=30,y=380)
 
-btn8=Button(locations,text="Location 8",height=5,width=30,background="red")
+btn8=Button(locations,text="Location 8",height=5,width=30,background="red",command=lambda:create_button("Location_8"))
 btn8.place(x=300,y=380)
 
-btn9=Button(locations,text="Location 9",height=5,width=30,background="red")
+btn9=Button(locations,text="Location 9",height=5,width=30,background="red",command=lambda:create_button("Location_9"))
 btn9.place(x=570,y=380)
 
-btn10=Button(locations,text="Location 10",height=5,width=30,background="red")
+btn10=Button(locations,text="Location 10",height=5,width=30,background="red",command=lambda:create_button("Location_10"))
 btn10.place(x=30,y=510)
 
-btn11=Button(locations,text="Location 11",height=5,width=30,background="red")
+btn11=Button(locations,text="Location 11",height=5,width=30,background="red",command=lambda:create_button("Location_11"))
 btn11.place(x=300,y=510)
 
-btn12=Button(locations,text="Location 12",height=5,width=30,background="red")
+btn12=Button(locations,text="Location 12",height=5,width=30,background="red",command=lambda:create_button("Location_12"))
 btn12.place(x=570,y=510)
 
 lbl3=Label(locations,text="Locations",font=("courier",19,"bold"))
--- database.py
+++ database.py
@@ -15,26 +15,32 @@ def user_table():
 def manager_table():
     conn=makeconnection()
     c=conn.cursor()
-    c.execute('''CREATE TABLE IF NOT EXISTS managers(
-              id TEXT PRIMARY KEY NOT NULL,
-              password TEXT NOT NULL)''')
+    try:
+        c.execute("SELECT * FROM managers")
+
+    except:
+        c.execute('''CREATE TABLE IF NOT EXISTS managers(
+                id TEXT NOT NULL,
+                password TEXT NOT NULL)''')
+        print("run")
+        c.execute('''INSERT INTO managers(id,password) VALUES(?,?)
+                  ''',("admin","admin@123"))
     conn.commit()
     conn.close()
 
-def availability_table():
+def location_table(location):
     conn=makeconnection()
     c=conn.cursor()
     try:
-        c.execute("SELECT * FROM availability")
+        c.execute(f"SELECT * FROM {location}")
 
     except :
         lst=[('1','TRUE'),('2','TRUE'),('3','TRUE'),('4','TRUE'),('5','TRUE'),('6','TRUE'),('7','TRUE'),('8','TRUE'),('9','TRUE'),
              ('10','TRUE'),('11','TRUE'),('12','TRUE'),('13','TRUE'),('14','TRUE'),('15','TRUE'),('16','TRUE'),('17','TRUE'),('18','TRUE'),
              ('19','TRUE'),('20','TRUE'),('21','TRUE'),('22','TRUE'),('23','TRUE'),('24','TRUE'),('25','TRUE'),('26','TRUE'),('27','TRUE'),
              ('28','TRUE'),('29','TRUE'),('30','TRUE'),('31','TRUE'),('32','TRUE')]
-        c.execute("CREATE TABLE availability(id TEXT PRIMARY KEY NOT NULL,status TEXT NOT NULL,model TEXT,number TEXT)")
-        print("A")
-        c.executemany("INSERT INTO availability(id,status) VALUES(?,?)",lst)
+        c.execute(f"CREATE TABLE {location}(id TEXT PRIMARY KEY NOT NULL,status TEXT NOT NULL,model TEXT,number TEXT)")
+        c.executemany(f"INSERT INTO {location}(id,status) VALUES(?,?)",lst)
 
     conn.commit()
     conn.close()
